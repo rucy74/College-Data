feaf59bcf5648a48504eb5d673bd04328a4415fc
HyunGyu <smartboy1122@naver.com>
2016-09-10 00:09:53 +0900

    9/20 desktop

---
+++ 자료구조의 기초/문제 풀이/1단원/function_class_1.h
@@ -0,0 +1,34 @@
+#ifndef FUNCTION_CLASS_1_H
+#define FUNCTION_CLASS_1_H
+#endif // !FUNCTION_CLASS_1_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quickSort(int *, int, int);
+int factorial(int);
+bool isMultiple(long, long); // 1.8 Excercises R-1.8
+void printArray(int**, int, int); // 1.8 Excercises R-1.9 
+void f(int); // 1.8 Excercises R-1.10
+void g(int &);
+class flower; // 1.8 Excercises R-1.11
+class CreditCard; // 1.8 Excercises R-1.12~16
+ostream& operator<<(ostream&, const CreditCard&);
+bool isTwoPower(int); // 1.8 Excercises R-1.19 : I was wrong. <<<<<<<<<<<<<<<<<
+bool isIntHashset(vector<int>); // 1.8 Excercises C-1.3
+void printOddNumInVector(vector<int>); // 1.8 Excercises C-1.4
+void shuffle(int*, int); // 1.8 Excercises C-1.5
+void possibleStrings(string); // 1.8 Excercises C-1.6
+void possibleStringsFuction(char*, int, int);
+template<class T> // 1.8 Excercises C-1.8
+T innerProduct(vector<T>, vector<T>);
+class Vector2; // 1.8 Excercises C-1.9
+ostream &operator<<(ostream&, Vector2);
+long twoPower(int i); // 1.8 Excercises C-1.10
+int GCD(int,int); // 1.8 Excercises C-1.11
+string randomTyposGenerator(string); // 1.8 Excercises P-1.1
+class Calendar; // 1.8 Excercises P-1.2
+// 1.8 Excercises P-1.3 나중에 해야지
+
+
